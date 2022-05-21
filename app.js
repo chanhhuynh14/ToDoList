4d20f0a23b6782d5a4f85d40bfe96d6964df6194
Huynh Chanh <34132931+chanhhuynh14@users.noreply.github.com>
2022-05-21 11:57:57 +0700

    add style

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 app.get("/",function(req,res){ 
     res.render("list",{newListItems : items});
 });
