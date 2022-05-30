a0291caea77b2b788b1ae722ba8c1f97b0ec01f5
Huynh Chanh <34132931+chanhhuynh14@users.noreply.github.com>
2022-05-30 21:22:40 +0700

    add mongoose

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const date = require(__dirname + "/date.js");
 
 var items = [];
 const app = express();
 
 app.set('view engine','ejs');
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongoose://localhost:27017/todolistDB", {useNewUrlParser: true});
+
 app.get("/",function(req,res){ 
     res.render("list",{newListItems : items});
 });
